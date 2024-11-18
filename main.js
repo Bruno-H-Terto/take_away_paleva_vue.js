160c82e58eeaf030dca11fd5016f86da0118399a
Bruno-H-Terto <herculanoterto@gmail.com>
2024-11-18 01:41:38 -0300

    Concluído ciclo de confirmação e finalização de pedidos

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const app = Vue.createApp({
       selected: '',
       Orders: [],
       message_orders: '',
-      Order: ''
+      Order: '',
+      message_order: ''
     }
   },
 
@@ -19,28 +20,36 @@ const app = Vue.createApp({
     }
   },
 
-  computed:{
-
+  watch: {
+    async selected(newVal, oldVal) {
+      if (newVal) {
+        await this.getOrders();
+      }
+    }
   },
 
   methods: {
     async getStore(code){
-      this.message = '';
-      data = await fetch('http://localhost:3000/api/v1/stores/'+code)
-      result = await data.json();
+      try{
+        this.message = '';
+        let data = await fetch('http://localhost:3000/api/v1/stores/'+code)
+        let result = await data.json();
 
-      if(result.error_message){
-        this.message = result.error_message;
-      }else{
-        store = result.store
-        var store_data = new Object();
-        store_data.trade_name = store.trade_name;
-        store_data.corporate_name = store.corporate_name;
-        store_data.register_number = store.register_number;
-        store_data.code = store.code;
-        sessionStorage.setItem("code", store.code);
-        this.current_store = store_data;
-        this.message = 'Acesso realizado com sucesso'
+        if(result.error_message){
+          this.message = result.error_message;
+        }else{
+          let store = result.store
+          var store_data = new Object();
+          store_data.trade_name = store.trade_name;
+          store_data.corporate_name = store.corporate_name;
+          store_data.register_number = store.register_number;
+          store_data.code = store.code;
+          sessionStorage.setItem("code", store.code);
+          this.current_store = store_data;
+          this.message = 'Acesso realizado com sucesso'
+        }
+      }catch{
+        this.message = 'Não foi possível conectar ao servidor'
       }
     },
 
@@ -51,30 +60,35 @@ const app = Vue.createApp({
     },
 
     async getOrders(){
-      this.Orders = [];
-      this.orders = '';
-      let params = new URLSearchParams({
-        status: this.selected,
-      });
-
-      let data = await fetch(`http://localhost:3000/api/v1/stores/${this.current_store.code}/orders/status?${params}`);
-      results = await data.json();
-      if(results.message){
-        this.message_orders = results.message
-      }else{
-        this.message_orders = '';
-        results.forEach(order => {
-          var data_order = new Object();
-          data_order.code = order.code;
-          data_order.name = order.name;
-          data_order.status = order.status;
-
-          this.Orders.push(data_order);
+      try{
+        this.Orders = [];
+        this.orders = '';
+        let params = new URLSearchParams({
+          status: this.selected,
         });
+
+        let data = await fetch(`http://localhost:3000/api/v1/stores/${this.current_store.code}/orders/status?${params}`);
+        let results = await data.json();
+        if(results.message){
+          this.message_orders = results.message
+        }else{
+          this.message_orders = '';
+          results.forEach(order => {
+            var data_order = new Object();
+            data_order.code = order.code;
+            data_order.name = order.name;
+            data_order.status = order.status;
+
+            this.Orders.push(data_order);
+          });
+        }
+      }catch{
+        this.message_orders = 'Não foi possível conectar ao servidor'
       }
     },
 
     async showModal(order_code){
+      this.message_order = '';
       const modal = document.querySelector(`#order_details`);
       Order = await this.getDetailsOrder(order_code);
       modal.showModal();
@@ -82,30 +96,72 @@ const app = Vue.createApp({
 
     async getDetailsOrder(order_code){
       let data = await fetch(`http://localhost:3000/api/v1/stores/${this.current_store.code}/orders/${order_code}`);
-      result = await data.json();
+      let result = await data.json();
       if(result.error_message){
         this.message_order = result.error_message;
       }else{
-        var order = new Object();
-        order.code = result.order.code;
-        order.name = result.order.name;
-        order.phone_number = result.order.phone_number || 'Sem registro';
-        order.email = result.order.email || 'Sem registro';
-        order.status = result.order.status;
-        order.register_number = result.order.register_number || 'Sem registro';
-        order.created_at = result.order.created_at_current;
-        order.items = [];
-        result.order_items.forEach(result_item => {
-          var item = new Object();
-          item.menu = result_item.menu;
-          item.item = result_item.item;
-          item.portion = result_item.portion;
-          item.observation = result_item.observation;
-          item.quantity = result_item.quantity;
-          order.items.push(item);
+        this.orderDetails(result);
+      }
+    },
+
+    orderDetails(result){
+      var order = new Object();
+      order.code = result.order.code;
+      order.name = result.order.name;
+      order.phone_number = result.order.phone_number || 'Sem registro';
+      order.email = result.order.email || 'Sem registro';
+      order.status = result.order.status;
+      order.register_number = result.order.register_number || 'Sem registro';
+      order.created_at = result.order.created_at_current;
+      order.items = [];
+      result.order_items.forEach(result_item => {
+        var item = new Object();
+        item.menu = result_item.menu;
+        item.item = result_item.item;
+        item.portion = result_item.portion;
+        item.observation = result_item.observation;
+        item.quantity = result_item.quantity;
+        order.items.push(item);
+      });
+    this.Order = '';
+    return this.Order = order;
+    },
+
+    async patchPreparing(order_code) {
+      try {
+        let response = await fetch(`http://localhost:3000/api/v1/stores/${this.current_store.code}/orders/${order_code}/confirmed`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ status: 'preparing' }),
         });
-      this.Order = '';
-      return this.Order = order;
+        if (!response.ok) throw new Error('Erro ao confirmar o pedido.');
+    
+        this.message_order = 'Pedido confirmado com sucesso!'
+        let result = await response.json();
+        this.Order = this.orderDetails(result);
+      } catch (error) {
+        console.error(error);
+      }
+    },  
+    
+    async patchDone(order_code) {
+      try {
+        let response = await fetch(`http://localhost:3000/api/v1/stores/${this.current_store.code}/orders/${order_code}/done`, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ status: 'done' }),
+        });
+        if (!response.ok) throw new Error('Erro ao confirmar o pedido.');
+    
+        this.message_order = 'Pedido concluído com sucesso!'
+        let result = await response.json();
+        this.Order = this.orderDetails(result);
+      } catch (error) {
+        console.error(error);
       }
     }
   }
